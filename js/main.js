321d6994efa53b573d900833ba97fb14cacae6f1
Daniel Kwon <danielkwon89@Daniels-MacBook-Pro-24897.local>
2021-11-26 17:20:59 -0500

    fleshed out renderAppName and renderCourses functions"

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const catalogEndpoint = 'https://s3-us-west-1.amazonaws.com/assets.interplay-lea
 document.addEventListener('DOMContentLoaded', () => {
     // making API call once DOM is loaded
     fetchCourses()
-    
+
 });
 
 // defining function to make API call
@@ -12,16 +12,49 @@ function fetchCourses() {
     .then(response => response.json())
     .then(data => {
         // call function here to render app name at top of page
-        console.log(data.app)
+        renderAppName(data.app)
         // call function here to render course information
-        console.log(data.courses)
+        renderCourses(data.courses)
     });
 }
 
-function renderAppName() {
-
+function renderAppName(appName) {
+    // creating h1 element for app name header and appending to DOM
+    const header = document.createElement('h1')
+    header.id = 'app-name-header'
+    header.innerHTML = appName
+    document.body.appendChild(header)
 }
 
-function renderCourses() {
+function renderCourses(courses) {
+    // rendering courses container div element
+    const coursesContainer = document.createElement('div')
+    coursesContainer.id = 'courses-container'
+    document.body.appendChild(coursesContainer)
 
+    courses.forEach(course => {
+        // creating a div element for each course
+        const courseDiv = document.createElement('div')
+        courseDiv.id = 'course'
+
+        // creating h3, img, and p elements for course title, image, and description, respectively
+        const courseName = document.createElement('h3')
+        courseName.innerText = course.name
+        courseName.id = 'course-header'
+
+        const courseImage = document.createElement('img')
+        courseImage.id = 'course-img'
+        courseImage.src = course.screenshot
+
+        const courseDescription = document.createElement('p')
+        courseDescription.innerText = course.description
+
+        // appending h3, img, and p elements to course div
+        courseDiv.appendChild(courseName)
+        courseDiv.appendChild(courseImage)
+        courseDiv.appendChild(courseDescription)
+
+        // appending course div to courses container
+        coursesContainer.appendChild(courseDiv)
+    })
 }
