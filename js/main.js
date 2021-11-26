8bde65d92ae3eb4e9366fb4c7daae280ee9099fa
Daniel Kwon <danielkwon89@Daniels-MacBook-Pro-24897.local>
2021-11-26 16:04:18 -0500

    coded fetch request to API endpoint. logged the response to console successfully

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-console.log("If you can see this, it's working!")
+// fetch request to API endpoint
+
+fetch('https://s3-us-west-1.amazonaws.com/assets.interplay-learning.com/interview-tests/simple-catalog/catalog.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
