3a373815866977ffdd02c80822bff987b5c96696
Daniel Kwon <danielkwon89@Daniels-MacBook-Pro-24897.local>
2021-11-26 16:35:11 -0500

    defined functions to fetch courses from API endpoint, and to render the app name and course information

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
-// fetch request to API endpoint
+const catalogEndpoint = 'https://s3-us-west-1.amazonaws.com/assets.interplay-learning.com/interview-tests/simple-catalog/catalog.json'
 
-fetch('https://s3-us-west-1.amazonaws.com/assets.interplay-learning.com/interview-tests/simple-catalog/catalog.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
+document.addEventListener('DOMContentLoaded', () => {
+    // making API call once DOM is loaded
+    fetchCourses()
+    
+});
+
+// defining function to make API call
+function fetchCourses() {
+    fetch(catalogEndpoint)
+    .then(response => response.json())
+    .then(data => {
+        // call function here to render app name at top of page
+        console.log(data.app)
+        // call function here to render course information
+        console.log(data.courses)
+    });
+}
+
+function renderAppName() {
+
+}
+
+function renderCourses() {
+
+}
